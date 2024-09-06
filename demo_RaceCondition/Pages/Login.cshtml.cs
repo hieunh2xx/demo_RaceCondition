6d18ca9eb743a9fce447e93155c9c99447154a95
S7venKing <you@example.com>
2024-09-06 09:28:44 +0700

    update limit login-time

--- demo_RaceCondition/Pages/Login.cshtml.cs
+++ demo_RaceCondition/Pages/Login.cshtml.cs
@@ -7,6 +7,8 @@ namespace demo_RaceCondition.Pages
     {
         private readonly UserService _userService;
 
+        private const int timeLoginLimit = 10;
+
         public LoginModel(UserService userService)
         {
             _userService = userService;
@@ -21,12 +23,29 @@ namespace demo_RaceCondition.Pages
             var user = _userService.Login(Email, Password);
             if (user == null)
             {
-                ModelState.AddModelError("", "Invalid login attempt.");
+                string message = UpdateLoginTimeAttemp() > 10 ? "You haved login fail for many time, try again after " : "Invalid login attempt.";
+                ViewData["Mesage"] = message;
+                ModelState.AddModelError("", message);
                 return Page();
             }
             HttpContext.Session.SetInt32("CurrentUserId", user.Id);
             HttpContext.Session.SetString("CurrentUserName", user.Name.ToString());
             return RedirectToPage("/Index");
+        }
+
+        public int UpdateLoginTimeAttemp()
+        {
+            int? timeLogin = HttpContext.Session.GetInt32("LoginTimeAttemp");
+            if (timeLogin == null)
+            {
+                HttpContext.Session.SetInt32("LoginTimeAttemp", 1);
+                return 1;
+            }
+            else
+            {
+                HttpContext.Session.SetInt32("LoginTimeAttemp", timeLogin.Value + 1);
+                return timeLogin.Value + 1;
+            }
         }
     }
 }
